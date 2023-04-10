822978921deedf3e73e86cce3cbb4ddec28df70e
iarseny <122807698+iarseny@users.noreply.github.com>
2023-04-10 20:34:32 +0300

    Create main.cpp

--- lesson 1/main.cpp
+++ lesson 1/main.cpp
@@ -1,1 +1,22 @@
+#include <vector>
+#include <map>
+#include <iostream>
 
+using namespace std;
+
+int main()
+{   
+    int sum = 9;
+    vector<int> a = {5, 4, 8, -1, 0};
+    map<int, int> m;
+
+    for (int i = 0; i < a.size(); i++) {
+        if (m.find(sum - a[i]) != m.end()) {
+            cout << i << " " << m[sum - a[i]];
+            break;
+        }
+        m[a[i]] = i;
+    }
+    
+    return 0;
+}

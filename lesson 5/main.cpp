230e16b599d1088f39f3f4d880e9f1dcb8454f79
iarseny <122807698+iarseny@users.noreply.github.com>
2023-05-28 16:17:36 +0300

    Update main.cpp

--- lesson 5/main.cpp
+++ lesson 5/main.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
+
+int n;
+void all_numbers(vector<int> s) {
+    if (s.size() == n) {
+        for (auto i : s) {
+            cout << i << " ";
+        }
+        cout << endl;
+        return;
+    }
+    
+    for (int i = 1; i <= n; i++) {
+        bool flag = true;
+        for (auto j : s) {
+            if (j == i) {
+                flag = false;
+            }
+        }
+        
+        if (flag) {
+            s.push_back(i);
+            all_numbers(s);
+            s.pop_back();
+        }
+    }
+}
+
+int main()
+{
+    int k;
+    cin >> k;
+    n = k;
+    all_numbers(vector<int> ());
+
+    return 0;
+}

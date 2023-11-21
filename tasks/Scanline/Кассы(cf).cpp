e4e7679cff80734a1c168e98990b8fa5150bc79e
iarseny <122807698+iarseny@users.noreply.github.com>
2023-11-21 20:18:35 +0000

    hello

---
+++ tasks/Scanline/Кассы(cf).cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> time;
+
+    for (int i = 0; i < n; i++) {
+        int f, s, t;
+        cin >> f >> s >> t;
+
+        time.push_back(f);
+    }
+
+    return 0;
+}
